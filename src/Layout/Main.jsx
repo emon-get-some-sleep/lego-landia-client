9750f297a14c0eb8f0449b107dbcbcd112ad0264
emonh0que <you@example.com>
2023-05-18 00:00:58 +0600

    navbar is done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavigationBar from '../Shared/NavigationBar/NavigationBar';
 
 const Main = () => {
     return (
         <div>
+            <NavigationBar></NavigationBar>
             <h2 className="text-2xl">This is Main</h2>
         </div>
     );
---
+++ src/Shared/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import logo from "/favicon.png";
+import { Link } from "react-router-dom";
+const NavigationBar = () => {
+  return (
+    <div className="flex items-center justify-around mt-[40px]">
+      <div className="flex items-center">
+        <img className="w-[40px] h-[70px]" src={logo} alt="" />
+        <h2 className="font-bold text-2xl">Lego Landia</h2>
+      </div>
+
+      <div className="space-x-3">
+        <Link to='/' className="font-semibold text-[#141414] hover:text-violet-600 duration-300">Home</Link>
+        <Link to='/'  className="font-semibold text-[#141414] hover:text-violet-600 duration-300">About Us</Link>
+        <Link to='/'  className="font-semibold text-[#141414] hover:text-violet-600 duration-300">Contact</Link>
+      </div>
+      <div>
+        <button className="bg-violet-400 px-6 py-3 rounded-lg text-white">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
