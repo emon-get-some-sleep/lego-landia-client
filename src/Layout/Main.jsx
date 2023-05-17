821a06d61f345375aec842ed143663ffba6ee7f1
emonh0que <you@example.com>
2023-05-17 23:36:51 +0600

    added routes

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">This is Main</h2>
+        </div>
+    );
+};
+
+export default Main;
