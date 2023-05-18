98088d461699331d9242c7557f32a5551453cc4c
emonh0que <you@example.com>
2023-05-18 15:43:46 +0600

    banner section done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavigationBar from '../Shared/NavigationBar/NavigationBar';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <NavigationBar></NavigationBar>
-            <h2 className="text-2xl">This is Main</h2>
+            <Outlet></Outlet>
         </div>
     );
 };
