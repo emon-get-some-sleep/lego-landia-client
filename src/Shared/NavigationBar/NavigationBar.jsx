e1508071fba15f21b5429856e6c63211a064290a
emonh0que <you@example.com>
2023-05-20 17:45:00 +0600

    navbar is done

--- src/Shared/NavigationBar/NavigationBar.jsx
+++ src/Shared/NavigationBar/NavigationBar.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from "react";
-
-import logo from "/favicon.png";
+import React, { useContext, useState } from "react";
+import { FaBars} from "react-icons/fa";
+import logo from "/favicon2.png";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import Swal from "sweetalert2";
 const NavigationBar = () => {
   const {user, logOut} = useContext(AuthContext)
+  const [showMenu, setShowMenu] = useState(false);
   const handleLogOut = () => {
     logOut();
     Swal.fire(
@@ -15,39 +16,62 @@ const NavigationBar = () => {
     )
   }
   return (
-    <div className="flex items-center justify-around mt-[40px]">
+    <div>
+    <div className="flex  items-center justify-between mt-[40px]">
       <div className="flex items-center">
         <img className="w-[40px] h-[70px]" src={logo} alt="" />
         <h2 className="font-bold text-2xl">Lego Landia</h2>
       </div>
 
-      <div className="space-x-3">
+      <div className="space-x-3 hidden md:block">
         <Link to='/' className="font-semibold text-[#141414] hover:text-violet-600 duration-300">Home</Link>
         <Link to='/'  className="font-semibold text-[#141414] hover:text-violet-600 duration-300">About Us</Link>
         <Link to='/'  className="font-semibold text-[#141414] hover:text-violet-600 duration-300">Contact</Link>
+        <Link to='/alltoys'  className="font-semibold text-[#141414] hover:text-violet-600 duration-300">All Toys</Link>
       </div>
-      {/* <div>
-        {
-          !user ?
-          <Link to="/login">
-        <button className="bg-violet-400 px-6 py-3 rounded-lg text-white">Login</button>
-        </Link>
-        :
-        <button onClick={handleLogOut}>Log out</button>
-        }
-        
-      </div> */}
-      <div className="tooltip hidden md:flex gap-3" data-tip={user?.displayName}>
+      
+      <div className="tooltip hidden md:flex items-center gap-3" data-tip={user?.displayName}>
         {!user ?
-        <Link to="/login"><button className="hidden md:block bg-[#3A1C36] text-white font-medium text-xl border px-6 py-4 rounded-lg">Login</button></Link>
+        <Link to="/login"><button className="hidden md:block bg-[#12AEE0]  text-white font-medium text-xl border px-8 py-3 hover:bg-[#164f62]  rounded-lg">Login</button></Link>
         :
         <>
         <img src={user.photoURL} className="w-[50px] h-[50px] rounded-[50%]" alt="" />
-        <button onClick={handleLogOut} className="text-sm text-gray-700">Sign Out</button>
+        <button onClick={handleLogOut} className="text-sm px-3 hover:bg-[#104d63] font-bold h-[35px] text-white bg-[#12AEE0]">Sign Out</button>
+        <Link to="/addtoy">
+        <button className="text-sm px-3 hover:bg-[#104d63] font-bold h-[35px] text-white bg-[#12AEE0]">My Toys</button>
+        </Link>
+        <Link to="/addtoy">
+        <button className="text-sm px-3 hover:bg-[#104d63] font-bold h-[35px] text-white bg-[#12AEE0]">Add A Toy</button>
+        </Link>
+        
         </>
         }
         
       </div>
+      <div className="md:hidden">
+      <FaBars onClick={() => setShowMenu(!showMenu)} className="text-2xl"/>
+      </div>
+     
+    </div>
+    {showMenu && <div className="flex md:hidden flex-col gap-3 items-center"> 
+        
+        <Link className="font-medium text-xl" to="/">Home</Link>
+        <Link className="font-medium text-xl" to="/Blogs">Blogs</Link>
+        <Link className="font-medium text-xl" to="/about">About</Link>
+        <Link className="font-medium text-xl" to="/alltoys">All Toys</Link>
+        
+        {!user ?
+        <Link to="/login"><button className="bg-[#12AEE0]  text-white font-medium text-xl border px-6 py-4 rounded-lg">Login</button></Link>
+        :
+        <>
+        <img src={user.photoURL} className="w-[50px] h-[50px] rounded-[50%]" alt="" />
+        <Link to="/addtoy">Add A Toy</Link>
+        <Link to="/addtoy">My Toys</Link>
+        <button onClick={handleLogOut} className="text-sm bg-[#12AEE0] text-white">Sign Out</button>
+        </>
+        }
+        
+    </div>}
     </div>
   );
 };
