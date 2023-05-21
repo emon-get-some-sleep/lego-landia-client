b1e3dce3e9c7d26bbf2252a5d5b82d53547429e0
emonh0que <you@example.com>
2023-05-21 12:18:52 +0600

    used loader

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,13 +1,19 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
+import { ClockLoader } from "react-spinners";
 
 
 const PrivateRoutes = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
+    const {user, isLoading} = useContext(AuthContext);
     const location = useLocation();
-    if(loading){
-        return <progress className="progress w-56"></progress>
+    if(isLoading){
+        return (
+            <div className="h-[100vh] flex items-center flex-col justify-center">
+                <h1 className="text-[#181D4E] text-3xl my-4">Loading Please Wait</h1>
+                <ClockLoader size='150' color="#36d7b7" />
+            </div>
+        )
     }
     if(user?.email){
         return children;
