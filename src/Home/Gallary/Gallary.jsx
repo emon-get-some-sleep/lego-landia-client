e5a942dba718b8664dc763bab4089cd852287c2d
emonh0que <you@example.com>
2023-05-21 22:15:38 +0600

    made category dynamic

--- src/Home/Gallary/Gallary.jsx
+++ src/Home/Gallary/Gallary.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Gallary.css";
 import SingleGallary from "./SingleGallary";
+
 const Gallary = () => {
+    const [gallary, setGallary] = useState([]);
+    useEffect(() => {
+      fetch('https://lego-landia-server-emon-get-some-sleep.vercel.app/gallary')
+      .then(res => res.json())
+      .then(data => setGallary(data))
+    }, [])
+
   return (
     <div className="flex items-center justify-center flex-col ">
       <h1 className="text-center text-[#181D4E] text-sm font-bold mb-3">Gallary</h1>
       <h1 className="text-center text-[#181D4E] text-[26px] md:text-[49px] font-bold mb-3">Our Gallary is Filled With Toys That Every Kid Will Absolutely Love</h1>
       <div className="grid mt-[40px] px-3 md:grid-cols-2 lg:grid-cols-3 gap-[30px]">
-        <SingleGallary></SingleGallary>
-        <SingleGallary></SingleGallary>
-        <SingleGallary></SingleGallary>
-        <SingleGallary></SingleGallary>
-        <SingleGallary></SingleGallary>
+       {
+        gallary.map(each => <SingleGallary key={each._id} each={each}></SingleGallary>)
+       }
       </div>
     </div>
   );
--- src/Home/Gallary/SingleGallary.jsx
+++ src/Home/Gallary/SingleGallary.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SingleGallary = () => {
+const SingleGallary = ({each}) => {
+    const {img, title, description} = each;
     return (
-        <div className='card-container w-full sm:w-[320px] h-[500px] overflow-hidden rounded-[20px] border-[8px] border-blue-600 relative'>
-            <img className='w-full h-full object-cover rounded-2xl' src="https://dispatch.barnesandnoble.com/content/dam/ccr/h/worldoflego/01012023/25631_LEGO_PCS_1-3_City_01-01.jpg" alt="" />
+        <div className='card-container w-full sm:w-[320px] h-[500px] overflow-hidden rounded-[20px] border-[8px]  relative'>
+            <img className='w-full h-full object-cover rounded-2xl' src={img} alt="" />
             <div id="card-description" className="card-description w-full h-full top-0 -right-[100%] duration-1000 absolute bg-[#181d4e25] backdrop-blur-sm rounded-2xl p-4 flex flex-col justify-center">
-                <h3 className='text-white font-semibold mx-auto text-[24px] md:text-4xl  my-5'>Lego City : Miniature</h3>
-                <p className='text-white text-center text-sm md:text-base font-medium'>Construct your very own city with us. A fun way to learn city architecture</p>
+                <h3 className='text-white font-semibold mx-auto text-[24px] md:text-4xl  my-5'>{title}</h3>
+                <p className='text-white  text-sm md:text-base font-medium'>{description}</p>
                 <button className='bg-[#12AEE0] w-full md:w-1/2 py-3 px-5 text-white rounded-lg mt-3 hover:bg-[#12ace077]'>View Details</button>
 
             </div>
